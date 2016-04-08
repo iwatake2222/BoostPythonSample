a4d394fb0b039b06ade31474dbda411ea522371e
take-iwiw <take.iwiw2222@gmail.com>
2016-04-08 00:00:24 -0400

    deleted using description

--- readme.md
+++ readme.md
@@ -55,20 +55,18 @@ print classA.setGetColor(MyModule.ClassA_COLOR.RED);
 #include "func.h"
 #include "ClassA.h"
 
-using namespace boost::python;
-
 BOOST_PYTHON_MODULE( MyModule )
 {
 	boost::python::def( "getNumber", getNumber );
 	boost::python::def( "setGetNumber", setGetNumber );
 	boost::python::def( "getString", getString );
 	boost::python::def( "getPoint", getPoint );
-	boost::python::class_<POINT>("POINT", init<>())
+	boost::python::class_<POINT>("POINT", boost::python::init<>())
 		.add_property("x", &POINT::x)
 		.add_property("y", &POINT::y)
 	;
 
-	boost::python::class_<ClassA::VECTOR_2D>("ClassA::VECTOR_2D", init<>())
+	boost::python::class_<ClassA::VECTOR_2D>("ClassA::VECTOR_2D", boost::python::init<>())
 		.add_property("x", &ClassA::VECTOR_2D::x)
 		.add_property("y", &ClassA::VECTOR_2D::y)
 	;
@@ -80,7 +78,7 @@ BOOST_PYTHON_MODULE( MyModule )
 	;
 
 
-	boost::python::class_<ClassA>("ClassA", init<int>())
+	boost::python::class_<ClassA>("ClassA", boost::python::init<int>())
 		.add_property("a", &ClassA::a)
 		.def("getA", &ClassA::getA)
 		.def("getVector", &ClassA::getVector)
--- src/MyModule.cpp
+++ src/MyModule.cpp
@@ -10,20 +10,18 @@ g++ -I/usr/include/python2.7 -fPIC -shared -o MyModule.so func.cpp ClassA.cpp My
 #include "func.h"
 #include "ClassA.h"
 
-using namespace boost::python;
-
 BOOST_PYTHON_MODULE( MyModule )
 {
 	boost::python::def( "getNumber", getNumber );
 	boost::python::def( "setGetNumber", setGetNumber );
 	boost::python::def( "getString", getString );
 	boost::python::def( "getPoint", getPoint );
-	boost::python::class_<POINT>("POINT", init<>())
+	boost::python::class_<POINT>("POINT", boost::python::init<>())
 		.add_property("x", &POINT::x)
 		.add_property("y", &POINT::y)
 	;
 
-	boost::python::class_<ClassA::VECTOR_2D>("ClassA::VECTOR_2D", init<>())
+	boost::python::class_<ClassA::VECTOR_2D>("ClassA::VECTOR_2D", boost::python::init<>())
 		.add_property("x", &ClassA::VECTOR_2D::x)
 		.add_property("y", &ClassA::VECTOR_2D::y)
 	;
@@ -35,7 +33,7 @@ BOOST_PYTHON_MODULE( MyModule )
 	;
 
 
-	boost::python::class_<ClassA>("ClassA", init<int>())
+	boost::python::class_<ClassA>("ClassA", boost::python::init<int>())
 		.add_property("a", &ClassA::a)
 		.def("getA", &ClassA::getA)
 		.def("getVector", &ClassA::getVector)
